13f3576b0a32cc396f8cc7a59eacdb55a04a4679
ngothithanhh <ngothanhthcstanhung@gmail.com>
2026-06-21 00:33:17 +0700

    feat: add project configuration files and implement language detection service

--- quizzlet/src/main/java/com/example/quizzlet/service/impl/LanguageDetectServiceImpl.java
+++ quizzlet/src/main/java/com/example/quizzlet/service/impl/LanguageDetectServiceImpl.java
@@ -19,26 +19,41 @@ import jakarta.annotation.PostConstruct;
 @Service
 @RequiredArgsConstructor
 public class LanguageDetectServiceImpl implements LanguageDetectService {
+
     private LanguageDetector detector;
 
     @PostConstruct
-    public void init() throws IOException {
-        List<LanguageProfile> languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+    public void init() {
+        try {
+            List<LanguageProfile> languageProfiles =
+                    new LanguageProfileReader().readAllBuiltIn();
 
-        this.detector = LanguageDetectorBuilder.create(NgramExtractors.standard())
-                .withProfiles(languageProfiles)
-                .build();
+            this.detector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+                    .withProfiles(languageProfiles)
+                    .build();
+
+        } catch (Exception e) {
+            this.detector = null;
+            System.err.println("Không khởi tạo được LanguageDetector: " + e.getMessage());
+        }
     }
 
     @Override
     public String detect(String text) {
+        if (text == null || text.isBlank()) {
+            return "en";
+        }
+
+        if (detector == null) {
+            return "en";
+        }
+
         Optional<LdLocale> lang = detector.detect(text);
 
         if (lang.isPresent()) {
             return lang.get().getLanguage();
         }
+
         return "en";
     }
-
-
 }
